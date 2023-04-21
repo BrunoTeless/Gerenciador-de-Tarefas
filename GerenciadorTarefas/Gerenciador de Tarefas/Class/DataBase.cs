a78e26708b530ba86e61072680b15fcf7e3bdd46
Bruno Teles <109414743+BrunoTeless@users.noreply.github.com>
2023-04-20 22:31:55 -0300

    Criação da Estrutura de Pastas!

---
+++ GerenciadorTarefas/Gerenciador de Tarefas/Class/DataBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerenciador_de_Tarefas.Class
+{
+        internal class DataBase
+        {
+            public string dbName;
+            public string Server;
+            public string user;
+            public string password;
+            public string connectionRow;
+
+            public string ConnectionnMake() 
+            {
+                this.connectionRow = $"Server={this.Server};Database={this.dbName};Uid={this.user};Pwd={this.password}";    
+                return connectionRow;
+            }
+        }
+    }
---
+++ GerenciadorTarefas/Gerenciador de Tarefas/forms/Form1.cs
@@ -0,0 +1,32 @@
+﻿using Gerenciador_de_Tarefas.Class;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gerenciador_de_Tarefas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataBase db = new DataBase();
+            db.dbName = "bdGerenciadorTarefas";
+            db.Server = "localhost";
+            db.user = "root";
+            db.password = "password";
+            db.ConnectionnMake();
+            MessageBox.Show(db.connectionRow, "Connection");
+        }
+    }
+}
